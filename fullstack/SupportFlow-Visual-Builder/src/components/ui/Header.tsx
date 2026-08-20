cdf3194dff6d9ec42269e53b56c63bc7b1d00e4b
victoire67 <ansimavicky@gmail.com>
2026-08-20 03:49:15 +0200

    ui: added the header's button feature(link to pages)

--- fullstack/SupportFlow-Visual-Builder/src/components/ui/Header.tsx
+++ fullstack/SupportFlow-Visual-Builder/src/components/ui/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { NavLink  , useParams} from "react-router";
+import { NavLink } from "react-router";
 function Header() {
 
     const [param , setParam] = useState('')
@@ -13,7 +13,7 @@ return (
             <header className="bg-[#FF9D42] w-full flex place-content-between px-20 py-4 items-center text-white ">
                 <h1 className="font-bold">SUPPORT FLOW</h1>
                 <NavLink to={location.pathname === "/preview" ? "/" : "preview"}>
-                    <button className="bg-white flex items-center text-[#FF9D42] rounded-lg p-2" onClick={changePath}>
+                    <button className="bg-white h- flex items-center text-[#FF9D42] rounded-lg p-2" onClick={changePath}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 11 11" fill="none">
                             <path d="M10.7097 5.57897L3.11133e-05 10.6908L0.315939 1.38079e-07L10.7097 5.57897Z" fill="#FF9D42" fill-opacity="0.78" />
                         </svg>
